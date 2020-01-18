0340a61042b0a48f53fc01fe8ccd09e37aa2af1f
fpezcara <fpezcara@gmail.com>
2020-01-18 20:30:40 +0000

    primer commit

---
+++ Consignas ejercicios objetos.md
@@ -0,0 +1,36 @@
+Ejercicio 1
+Crear una variable llamada user, a la que le vamos a asignar un objeto.
+El objeto tiene que tener 3 propiedades: firstName, lastName email, age, con tus datos
+Mostrar en consola el objeto user
+Mostrar en la consola el nombre y la edad de la persona: "Hola, mi nombre es [ACA EL NOMBRE DE PILA] y tengo [ACA LA EDAD] años"
+Ejercicio 2
+Tenemos un listado de personas y necesitamos guardarlo en una base de datos
+Los datos que tenemos son un id, email, nombre y teléfono
+Crear un objeto para cada persona que nos pasaron
+Mostrar por consola todos los objetos creados
+// DATOS DE LAS PERSONAS QUE NECESITAMOS GUARDAR EN UNA BASE DE DATOS
+// 1, ada@gmail.com, Ada Lovelace, 1234567890
+// 2, grace@hotmail.com, Grace Hopper, 0987654321
+// 3, hedy@gmail.com, Hedy Lamarr, 6789054321
+// 4, radia@yahoo.com, Radia Perlman, 1234509876
+// 5, sheryl@facebook.com, Sheryl Sandberg, 5432167890
+
+
+// Ejemplo del formato de cada objeto: 0,ejemplo@terra.com,Ejemplo,1029384756
+// const usuarioEjemplo = {
+//   id: 0,
+//   nombre: "Ejemplo",
+//   email: "ejemplo@terra.com",
+//   telefono: "1029384756"
+// }
+Ejercicio 3
+Con los objetos creados en el ejercicio anterior, mostrar en la consola los siguientes datos de cada persona:
+// 1. El nombre de Ada:
+
+// 2. El ID de Grace
+
+// 3. El email de Hedy
+
+// 4. El ID y nombre de Radia
+
+// 5. El telefono de Sheryl
---
+++ ejercicios-objetos.js
@@ -0,0 +1,69 @@
+//  ** EJERCICIO 1
+const user = {
+    firstName: "Florencia",
+    lastName: "Pezcara",
+    email: "fpezcara@gmail.com",
+    edad: 28,
+}
+
+console.log(user)
+
+console.log("Hola, mi nombre es " + user.firstName + " y tengo " + user.edad + " años")
+
+//  ** EJERCICIO 2
+
+const usuarioAda = {
+    id: 1,
+    email: "ada@gmail.com",
+    nombre: "Ada Lovelace",
+    telefono: 1234567890,
+}
+
+const usuarioGrace = {
+    id: 2,
+    email: "grace@hotmail.com",
+    nombre: "Grace Hopper",
+    telefono: 0987654321,
+}
+
+const usuarioHedy = {
+    id: 3,
+    email: "hedy@gmail.com",
+    nombre: "Hedy Lamarr",
+    telefono: 6789054321,
+}
+
+const usuarioRadia = {
+    id: 4,
+    email: "radia@yahoo.com",
+    nombre: "Radia Perlman",
+    telefono: 1234509876,
+}
+
+const usuarioSheryl = {
+    id: 5,
+    email: "sheryl@facebook.com",
+    nombre: "Sheryl Sandberg",
+    telefono: 5432167890,
+}
+
+
+// **EJERCICIO 3
+
+// Con los objetos creados en el ejercicio anterior, mostrar en la consola los siguientes datos de cada persona:
+// 1. El nombre de Ada: 
+console.log("1. El nombre de Ada: " + usuarioAda.nombre)
+
+// 2. El ID de Grace
+console.log("2. El ID de Grace: " + usuarioGrace.id)
+
+// 3. El email de Hedy
+console.log("3. El email de Hedy: " + usuarioHedy.email)
+
+// 4. El ID y nombre de Radia
+console.log("El ID y nombre de Radia: " + usuarioRadia.id + ", " + usuarioRadia.nombre)
+
+// 5. El telefono de Sheryl
+console.log("El teléfono de Sheryl: " + usuarioSheryl.telefono)
+
+
